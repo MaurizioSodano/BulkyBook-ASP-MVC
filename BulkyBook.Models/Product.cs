eadf8e8813c2553d65c06b9a829770d0f4ee913d
Maurizio Sodano <maurizio.sodano@gmail.com>
2023-03-28 18:02:06 +0200

    added Product model with FK

---
+++ BulkyBook.Models/Product.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BulkyBook.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public double ListPrice { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public double Price { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public double Price50 { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public double Price100 { get; set; }
+
+
+
+        public string ImageUrl { get; set; }
+
+        public int CategoryId { get; set; }
+
+        [ForeignKey(nameof(CategoryId))] //not required FK Category
+        public Category Category { get; set; }
+
+        public int CoverTypeId { get; set; }     //Automatic FK CoverType
+        public CoverType CoverType { get; set; }    //Automatic FK CoverType
+
+    }
+}

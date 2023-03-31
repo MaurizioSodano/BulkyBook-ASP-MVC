6834d194fc8d70c484f06a76462038b2f60cd025
Maurizio Sodano <maurizio.sodano@gmail.com>
2023-03-31 19:51:01 +0200

    updated book image

--- BulkyBook.Models/Product.cs
+++ BulkyBook.Models/Product.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -19,31 +20,38 @@ namespace BulkyBook.Models
         public string Author { get; set; }
 
         [Required]
+        [DisplayName("List Price")]
         [Range(1, 10000)]
         public double ListPrice { get; set; }
 
         [Required]
+        [DisplayName("Price for 1-50+")]
         [Range(1, 10000)]
         public double Price { get; set; }
 
         [Required]
+        [DisplayName("Price for 51-100")]
         [Range(1, 10000)]
         public double Price50 { get; set; }
 
         [Required]
+        [DisplayName("Price for 100+")]
         [Range(1, 10000)]
         public double Price100 { get; set; }
 
 
         [ValidateNever]
         public string ImageUrl { get; set; }
-
+        [Required]
+        [DisplayName("Category")]
         public int CategoryId { get; set; }
 
         [ForeignKey(nameof(CategoryId))] //not required FK Category
         [ValidateNever]
         public Category Category { get; set; }
 
+        [Required]
+        [DisplayName("Cover Type")]
         public int CoverTypeId { get; set; }     //Automatic FK CoverType
         [ValidateNever]
         public CoverType CoverType { get; set; }    //Automatic FK CoverType
--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -76,6 +76,16 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                     var uploads = Path.Combine(wwwRootPath, @"images\products");  //concatenates the folder path
                     var extension = Path.GetExtension(file.FileName); // get the original file Extension
 
+                    //check if there is an existing image
+                    if (obj.Product.ImageUrl != null)
+                    {
+                        var oldImagePath = Path.Combine(wwwRootPath, obj.Product.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
                     using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
                     {
                         file.CopyTo(fileStreams); //copy the file
